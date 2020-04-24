10131b58b0477252624daf16fd7048c2cefec20d
zhangqing <heyzqt@qq.com>
2020-04-24 18:14:39 +0800

    引入styled-components组件对style进行统一管理,引入Reset CSS统一不同浏览器标签的样式

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { GlobalStyle } from  "./style.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <React.Fragment>
+      <GlobalStyle />
+      <App />
+    </React.Fragment>
   </React.StrictMode>,
   document.getElementById("root")
 );
