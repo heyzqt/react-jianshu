fd6c3a008b40ab53f779e42f6ad5cbbf6711e6e7
zhangqing <heyzqt@qq.com>
2020-04-26 15:11:00 +0800

    引入immutable.js库,重写header的reducer中的state,防止数据被误修改

--- src/common/header/store/constants.js
+++ src/common/header/store/constants.js
@@ -1,3 +1,3 @@
-export const INPUT_GET_FOCUS = "input_get_focus";
+export const INPUT_GET_FOCUS = "header/input_get_focus";
 
-export const INPUT_LOSE_FOCUS = "input_lose_focus";
+export const INPUT_LOSE_FOCUS = "header/input_lose_focus";
